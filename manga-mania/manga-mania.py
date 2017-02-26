f993c98bdee745bb7a88d01dc7beb346aa0c9a2f
TheIllusionistMirage <lostsoulofthegeek@gmail.com>
2017-02-26 17:18:58 +0530

    Added GUI code

--- manga-mania/manga-mania.py
+++ manga-mania/manga-mania.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+import sys
+from PyQt5.QtWidgets import QApplication
+from gui import GUI
+
+
+
+if __name__ == '__main__':
+    
+    app = QApplication(sys.argv)
+    g = GUI()
+    sys.exit(app.exec_())
